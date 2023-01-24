b1caea503df8632c0fa5be10c61f86fc0f997470
Lucas Zunino <zunino10@hotmail.com>
2023-01-23 21:34:20 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,35 @@
-test('', () => {});
+import React from 'react';
+import { screen, act } from '@testing-library/react';
+import renderWithRouter from './helpers/renderWithRouter';
+import App from '../App';
+
+describe('Teste o componente <NotFound.js />', () => {
+  test('A página contém um heading h2 com o texto Page requested not found', () => {
+    const { history } = renderWithRouter(<App />);
+
+    act(() => {
+      history.push('/paginaInexistente');
+    });
+
+    const notFoundTitle = screen.getByRole(
+      'heading',
+      { name: 'Page requested not found' },
+    );
+    expect(notFoundTitle).toBeInTheDocument();
+  });
+
+  test('A página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    const { history } = renderWithRouter(<App />);
+
+    act(() => {
+      history.push('/paginaInexistente');
+    });
+
+    const notFoundImg = screen.getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i,
+    });
+
+    expect(notFoundImg).toBeInTheDocument();
+    expect(notFoundImg.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
